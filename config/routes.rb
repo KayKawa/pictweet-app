358daa936972ffce855920666a828b2752a68387
KayKawa <goodenoughliving1221@gmail.com>
2022-06-23 15:08:12 +0900

    コントローラー作成
    indexアクション

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :tweets,only: :index
   # Defines the root path route ("/")
