2e58c6b97b7299c0aac7d3b1eed952861c4f8a21
KayKawa <goodenoughliving1221@gmail.com>
2022-06-23 15:20:00 +0900

    rootディレクトリ設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
+  root to: 'tweets#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :tweets,only: :index
   # Defines the root path route ("/")
