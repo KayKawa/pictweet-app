e09322ca26cd8ebcdb1f50e1fbd9856f597615bb
KayKawa <goodenoughliving1221@gmail.com>
2022-06-24 10:56:59 +0900

    詳細機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'tweets#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :tweets,only: [:index,:new,:create,:destroy,:edit,:update]
+  resources :tweets
   # Defines the root path route ("/")
   # root "articles#index"
 end
