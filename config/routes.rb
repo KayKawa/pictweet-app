1a3976bf08c4d2691a02e9eaab8da8fe892eba64
KayKawa <goodenoughliving1221@gmail.com>
2022-06-25 14:28:40 +0900

    検索機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :tweets do
     resources :comments,only: :create
+    collection do
+      get 'search'
+    end
   end
   resources :users ,only: :show
   # Defines the root path route ("/")
