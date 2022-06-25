599016196e483fb211e8a261688f33dd6e73d115
KayKawa <goodenoughliving1221@gmail.com>
2022-06-25 13:37:40 +0900

    ルーティング（ネスト）

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :tweets
+  resources :tweets do
+    resources :comments,only: :create
+  end
   resources :users ,only: :show
   # Defines the root path route ("/")
   # root "articles#index"
